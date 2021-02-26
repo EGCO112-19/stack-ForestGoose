3aec86d12b5dfe2fed53de4cf893c40b07328f8a
ForestGoose <chayawat.pahn1910@gmail.com>
2021-02-26 08:02:00 +0000

    Edit stack.h and main.c

--- main.c
+++ main.c
@@ -3,27 +3,26 @@
 #include<string.h>
 #include "stack.h"
 
-int main(int argc, char **argv){
+int main(int argc,char** argv){
   printf("Checking the parentheses in argv arguments\n");
   int i,N,j;
   
   Stack s;
+  s.top = NULL;
+  s.size = 0;
 
-
- 
   for(i=1;i<argc;i++){
-   
-     for(j=0;j<strlen(argv[i]);j++){
-       /* Use stack to help with the parentheses*/
-
-
-     }
-
-
+    for(j=0;j<strlen(argv[i]);j++){
+      char c = argv[i][j];
+      if(c == '(' || c == '{' || c == '[')
+        push(&s,c);
+      if(c == ')' && pop(&s) != '(') break;
+      if(c == '}' && pop(&s) != '{') break;
+      if(c == ']' && pop(&s) != '[') break;
+    }
+    if(j == strlen(argv[i]) && s.size == 0) printf("True\n");
+    else printf("False\n");
+    pop_all(&s);
   }
-
-
-
-
-   return 0;
+  return 0;
 }
--- stack.h
+++ stack.h
@@ -8,7 +8,30 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(Stack s);
-void pop_all(Stack s);
+void push(StackPtr s,char value){
+  Node *new_node = (Node*)malloc(sizeof(Node));
+  if(new_node){
+    new_node->data = value;
+    new_node->nextPtr= s->top;
+    s->top=new_node;
+    s->size++;
+  }
+}
+char pop(StackPtr s){
+  if(s->top == NULL) return '\0';
+  NodePtr temp = s->top;
+  char value = temp->data;
+  s->top = temp->nextPtr;
+  s->size--;
+  free(temp);
+  return value;
+}
+void pop_all(StackPtr s){
+  while(s->size){
+    NodePtr temp = s->top;
+    s->top = temp->nextPtr;
+    s->size--;
+    free(temp);
+  }
+}
 #endif
